cf0584258a1f1aa02128977ad62b5852850d00e7
Dolevsol <dolevs@wix.com>
2022-12-14 17:29:34 +0200

    finish toggle

--- static/script.js
+++ static/script.js
@@ -1,6 +1,18 @@
-let commentWindow  ;
+let commentWindow;
 
 function openAddComment() {
-   commentWindow = window.open("comment.html", 'newWindow', 'width=600,height=600') ;
+    commentWindow = window.open("comment.html", 'newWindow', 'width=600,height=600');
 
 }
+
+function enableElement(id, enable) {
+    document.getElementById(id).disabled = !enable;
+}
+
+function disableCourse() {
+    document.getElementById('courseNumber').disabled = !document.getElementById('courseNumber').disabled;
+    document.getElementById('hardness').disabled = !document.getElementById('hardness').disabled;
+    document.getElementById('load').disabled = !document.getElementById('load').disabled;
+
+}
+
